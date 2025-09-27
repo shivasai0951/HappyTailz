87e5707e5f5237e5e5dc0178e3c6dacee807c1ab
Shiva Sai <sais31348@gmail.com>
2025-09-27 19:34:41 +0530

    user added keys

--- models/Plan.js
+++ models/Plan.js
@@ -5,6 +5,8 @@ const PlanSchema = new mongoose.Schema(
     name: { type: String, required: true, enum: ['1 day', '1 week', '1 month'], index: true },
     durationDays: { type: Number, required: true },
     price: { type: Number, required: true },
+    oldPrice: { type: Number },
+    discount: { type: Number },
     description: { type: String },
     active: { type: Boolean, default: true }
   },
